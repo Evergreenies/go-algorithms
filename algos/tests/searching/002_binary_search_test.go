5280114390633c0af97c7614570f6bb336e15e72
evergreenies <evergreenlief@gmail.com>
2023-07-26 21:22:00 +0530

    Adding Binary Search

--- algos/tests/searching/002_binary_search_test.go
+++ algos/tests/searching/002_binary_search_test.go
@@ -6,7 +6,7 @@ import (
 	search "github.com/Evergreenies/go-algorithms/algos/searching"
 )
 
-func TestLinearSearch(t *testing.T) {
+func TestBinarySearch(t *testing.T) {
 
 	arr := make([]int, 1000000)
 	for i := 0; i < 1000000; i++ {
@@ -18,20 +18,17 @@ func TestLinearSearch(t *testing.T) {
 		target         int
 		expected_index int
 	}{
-		{[]int{4, 2, 7, 1, 9, 5, 3}, 9, 4},
-		{[]int{4, 2, 7, 1, 9, 5, 3}, 8, -1},
 		{[]int{}, 9, -1},
 		{[]int{9}, 9, 0},
 		{[]int{6}, 9, -1},
-		{[]int{2, 4, 6, 8, 2, 10}, 2, 0},
-		{[]int{2, 4, 6, 8, 2, 10}, 9, -1},
+		{[]int{2, 4, 6, 8, 10}, 2, 0},
+		{[]int{2, 4, 6, 8, 10}, 9, -1},
 		{arr, 999999, 999999},
-		{[]int{-4, -2, -7, -1, -9, -5, -3}, -9, 4},
-		{[]int{-4, -2, -7, -1, -9, -5, -3}, -8, -1},
+		{[]int{0, 0, 0, 1, 3, 5, 7, 9, 11, 13}, 7, 6},
 	}
 
 	for index, test := range tests {
-		if actual_index := search.LinearSearch(test.input, test.target); actual_index != test.expected_index {
+		if actual_index := search.BinarySearch(test.input, test.target); actual_index != test.expected_index {
 			t.Errorf("FAILED: Test Case: %v \n Expected: %v \n Actual: %v \n", index, test.expected_index, actual_index)
 		} else {
 			t.Logf("PASSED: Test Case: %v \n Expected: %v \n Actual: %v \n", index, test.expected_index, actual_index)
--- main.go
+++ main.go
@@ -9,5 +9,9 @@ import (
 func main() {
 	arr := []int{-4, -2, -7, -1, -9, -5, -3}
 	target := -9
-	fmt.Println(search.LinearSearch(arr, target))
+	fmt.Println(search.BinarySearch(arr, target))
+
+	arr = []int{0, 0, 0, 1, 3, 5, 7, 9, 11, 13}
+	target = 7
+	fmt.Println(search.BinarySearch(arr, target))
 }
