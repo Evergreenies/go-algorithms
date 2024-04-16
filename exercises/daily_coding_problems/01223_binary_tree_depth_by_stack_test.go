d20c46223bbcb01efbc47b7609ea05067a6b69bc
evergreenies <evergreenlief@gmail.com>
2024-04-16 07:52:22 +0530

    added DCP01223 GO

---
+++ exercises/daily_coding_problems/01223_binary_tree_depth_by_stack_test.go
@@ -0,0 +1,54 @@
+package dailycodingproblems
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+/**
+* You are given a binary tree in a peculiar string representation. Each node is written in
+* the form (lr), where l corresponds to the left child and r corresponds to the right child.
+*
+* If either l or r is null, it will be represented as a zero. Otherwise, it will be represented
+* by a new (lr) pair.
+*
+* Here are a few examples:
+*
+* A root node with no children: (00)
+* A root node with two children: ((00)(00))
+* An unbalanced tree with three consecutive left children: ((((00)0)0)0)
+*
+* Given this representation, determine the depth of the tree.
+* **/
+
+func getMaxOfStack(depth int, stack []string) int {
+	stackLen := len(stack)
+	if depth > stackLen {
+		return depth
+	}
+
+	return stackLen
+}
+
+func binaryTreeMaxDepth(str string) int {
+	stack, depth := make([]string, 0), 0
+	for char := range str {
+		if string(char) == "(" {
+			stack = append(stack, string(char))
+			depth = getMaxOfStack(depth, stack)
+		} else if string(char) == ")" {
+			stack = stack[:len(stack)-1]
+		}
+	}
+
+	return depth
+}
+
+func TestMaxDepthOfTree(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.Equal(binaryTreeMaxDepth("(00)"), 1, "(00) this has just one node as root node")
+	assert.Equal(binaryTreeMaxDepth("((00)(00))"), 2, "((00)(00)) this must have 2 nodes only so depth would be 2")
+	assert.Equal(binaryTreeMaxDepth("((((00)0)0)0)"), 4, "((((00)0)0)0) it have depth of 4 only")
+}
